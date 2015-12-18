4a05a61c6f744aaf29458446ca389561c7ec6a02
jiftech <jiftech.stlfy@gmail.com>
2015-12-18 10:48:39 +0900

    modify comments

--- codegen.c
+++ codegen.c
@@ -429,7 +429,7 @@ codegen_expression_less (struct AST *ast)
   emit_code (ast, "\tpushl\t$1\t#真の場合\n");
   emit_code (ast, "L%d:\n", label2);
 
-  frame_height -= 4; /* スタックトップに比較結果が積まれた */
+  frame_height -= 4; /*　2回pop(-8)後,結果を積む(+4) */
 }
 
 static void
@@ -448,7 +448,7 @@ codegen_expression_add (struct AST *ast)
   emit_code (ast, "\taddl\t%%ecx, %%eax\t#加算\n");
   emit_code (ast, "\tpushl\t%%eax\t#結果をスタックに積む\n");
 
-  frame_height -= 4; /* スタックトップに加算結果が積まれた */
+  frame_height -= 4; /* 2回pop(-8)後,結果を積む(+4) */
   
 }
 
@@ -468,7 +468,7 @@ codegen_expression_sub (struct AST *ast)
   emit_code (ast, "\tsubl\t%%ecx, %%eax\t#減算\n");
   emit_code (ast, "\tpushl\t%%eax\t#結果をスタックに積む\n");
 
-  frame_height -= 4; /* スタックトップに減算結果が積まれた */
+  frame_height -= 4; /* 2回pop(-8)後,結果を積む(+4) */
   
 }
 
